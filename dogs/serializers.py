95a1803371698de36fef11b7f1b1cb87acb9c893
Эдуард Кусков <shadowfish@yandex.ru>
2024-07-10 21:45:37 +0300

    +HW_24training

--- dogs/serializers.py
+++ dogs/serializers.py
@@ -1,15 +1,36 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from dogs.models import Dog, Breed
 
 
+class BreedSerializer(ModelSerializer):
+    dogs = SerializerMethodField()
+
+    def get_dogs(self, obj):
+        return [dog.name for dog in Dog.objects.filter(breed=obj)]
+
+    class Meta:
+        model = Breed
+        fields = "__all__"
+
+
 class DogSerializer(ModelSerializer):
+    breed = BreedSerializer(read_only=True)
     class Meta:
         model = Dog
         fields = "__all__"
 
 
-class BreedSerializer(ModelSerializer):
+class DogDetailSerializer(ModelSerializer):
+    count_dog_with_same_breed = SerializerMethodField()
+    breed = BreedSerializer(read_only=True)
+
+    def get_count_dog_with_same_breed(self, obj):
+        return Dog.objects.filter(breed=obj.breed).count()
+
     class Meta:
-        model = Breed
-        fields = "__all__"
+        model = Dog
+        fields = ('name', 'breed', 'description', 'photo', 'date_born', 'count_dog_with_same_breed')
+
+
--- dogs/views.py
+++ dogs/views.py
@@ -1,13 +1,24 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from dogs.models import Dog, Breed
-from dogs.serializers import DogSerializer, BreedSerializer
+from dogs.serializers import DogSerializer, BreedSerializer, DogDetailSerializer
 
 
 class DogViewSet(ModelViewSet):
     queryset = Dog.objects.all()
-    serializer_class = DogSerializer
+    filterset_fields = ('breed',)
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ('date_born',)
+    search_fields = ('name',)
+
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return DogDetailSerializer
+        return DogSerializer
 
 
 class BreedCreateAPIView(CreateAPIView):
